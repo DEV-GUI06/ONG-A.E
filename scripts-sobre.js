fef0226cafa49cd5d862368bbc341f7aca94da48
Guilherme Batista <guilhermebsdeveloper@gmail.com>
2025-02-08 02:17:50 -0300

    Add files via upload

--- scripts-sobre.js
+++ scripts-sobre.js
@@ -60,3 +60,14 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   }
 });
+
+// Função para traduzir o texto
+function translateText(lang) {
+  const translations = {
+      "pt": "A ONG Amigos da Esperança compartilha do compromisso para com imigrantes, oferecendo assistência de encaminhamento na regularização de documentos para a comunidade imigrante e promovendo projetos que visam o bem-estar e o crescimento sustentável de todos. Convidamos você a se juntar a nós nessa missão, seja como voluntário ou parceiro, para que juntos possamos construir uma sociedade mais justa, diversa e acolhedora para todos.",
+      "en": "The NGO Amigos da Esperança shares the commitment to immigrants, offering assistance in document regularization for the immigrant community and promoting projects aimed at the well-being and sustainable growth of all. We invite you to join us in this mission, whether as a volunteer or partner, so that together we can build a fairer, more diverse, and welcoming society for all.",
+      "fr": "L'ONG Amigos da Esperança partage l'engagement envers les immigrants, offrant une assistance dans la régularisation des documents pour la communauté immigrée et en promouvant des projets visant le bien-être et la croissance durable de tous. Nous vous invitons à nous rejoindre dans cette mission, que ce soit en tant que bénévole ou partenaire, afin que nous puissions ensemble construire une société plus juste, plus diversifiée et plus accueillante pour tous."
+  };
+  
+  document.getElementById("text").innerText = translations[lang];
+}
